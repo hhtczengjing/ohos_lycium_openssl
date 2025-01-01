f6a347ed00946ae9999f6d43ec84c3c3784bb628
zengjing <hhtczengjing@gmail.com>
2025-01-01 17:57:43 +0800

    修改脚本

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # ohos_lycium_openssl
 
 ohos lycium openssl
+
+### Usage
+
+```
+git clone https://github.com/hhtczengjing/ohos_lycium_openssl.git --recurse-submodules
+sh +x ./build.sh
+```
--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
 
 export OHOS_SDK="$HOME/ohos/command-line-tools/sdk/default/openharmony"
 
 cp -r openssl_1_0_2k tpc_c_cplusplus/thirdparty
-cp envset.sh tpc_c_cplusplus/lycium
+cp envset.sh tpc_c_cplusplus/lycium/script
+
 cd tpc_c_cplusplus/lycium
 ./build.sh openssl_1_0_2k
